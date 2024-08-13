9b413956a540d8b042993d3ffb05f548f5d5a49d
LeChocolatChaud <mazihan1024@hotmail.com>
2024-08-13 10:41:59 +0800

    various changes - change color - add new levels - add animation

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ ball_pos = (1, 1)
 selected_lvl = 1
 mouse_prev_pressed = pygame.mouse.get_pressed()
 key_press_cd = 0
+direction = "up"
+layer_direction = "lower"
+animate_layer = False
+animate = False
 COOLDOWN = 10
 stage = "menu"
 ball_depth = 0
@@ -125,70 +129,139 @@ while running:
           return True
       return False
 
-    # draw maze
+    def generate_layer_map(screen: pygame.Surface):
+      # draw map
+      screen.fill("black")
 
-    screen.fill("black")
-
-    for y, row in enumerate(layer):
-      for x, cell in enumerate(row):
-        if cell == "#":
-          pygame.draw.rect(screen, "white", (x * 40, y * 40, 40, 40))
-        if cell == "x":
-          pygame.draw.circle(screen, "green", (x * 40 + 20, y * 40 + 20), 16)
-        if cell == " ":
-          if can_go_next_layer(x, y):
-            pygame.draw.rect(screen, "orange", (x * 40, y * 40, 40, 40))
-
-    # draw ball
-    pygame.draw.circle(screen, "dodgerblue",
-                       (ball_pos[0] * 40 + 20, ball_pos[1] * 40 + 20), 16)
-
-    # draw depth
-    if ball_depth > 0:
-      prev_layer = lvl[ball_depth - 1]
-      for y, row in enumerate(prev_layer):
+      for y, row in enumerate(layer):
         for x, cell in enumerate(row):
           if cell == "#":
-            if x > 0 and prev_layer[y][x - 1] != "#":
-              pygame.draw.line(screen, "gray", (x * 40, y * 40),
-                               (x * 40, y * 40 + 5), 3)
-              pygame.draw.line(screen, "gray", (x * 40, y * 40 + 15),
-                               (x * 40, y * 40 + 25), 3)
-              pygame.draw.line(screen, "gray", (x * 40, y * 40 + 35),
-                               (x * 40, y * 40 + 40), 3)
-            if x < 14 and prev_layer[y][x + 1] != "#":
-              pygame.draw.line(screen, "gray", (x * 40 + 40, y * 40),
-                               (x * 40 + 40, y * 40 + 5), 3)
-              pygame.draw.line(screen, "gray", (x * 40 + 40, y * 40 + 15),
-                               (x * 40 + 40, y * 40 + 25), 3)
-              pygame.draw.line(screen, "gray", (x * 40 + 40, y * 40 + 35),
-                               (x * 40 + 40, y * 40 + 40), 3)
-            if y > 0 and prev_layer[y - 1][x] != "#":
-              pygame.draw.line(screen, "gray", (x * 40, y * 40),
-                               (x * 40 + 5, y * 40), 3)
-              pygame.draw.line(screen, "gray", (x * 40 + 15, y * 40),
-                               (x * 40 + 25, y * 40), 3)
-              pygame.draw.line(screen, "gray", (x * 40 + 35, y * 40),
-                               (x * 40 + 40, y * 40), 3)
-            if y < 14 and prev_layer[y + 1][x] != "#":
-              pygame.draw.line(screen, "gray", (x * 40, y * 40 + 40),
-                               (x * 40 + 5, y * 40 + 40), 3)
-              pygame.draw.line(screen, "gray", (x * 40 + 15, y * 40 + 40),
-                               (x * 40 + 25, y * 40 + 40), 3)
-              pygame.draw.line(screen, "gray", (x * 40 + 35, y * 40 + 40),
-                               (x * 40 + 40, y * 40 + 40), 3)
+            pygame.draw.rect(screen, "darkgray", (x * 40, y * 40, 40, 40))
+          if cell == "x":
+            pygame.draw.circle(screen, "green", (x * 40 + 20, y * 40 + 20), 16)
+          if cell == " ":
+            if can_go_next_layer(x, y):
+              pygame.draw.rect(screen, "orange", (x * 40, y * 40, 40, 40))
 
+      # draw depth
+      if ball_depth > 0:
+        prev_layer = lvl[ball_depth - 1]
+        for y, row in enumerate(prev_layer):
+          for x, cell in enumerate(row):
+            if cell == "#":
+              if x > 0 and prev_layer[y][x - 1] != "#":
+                pygame.draw.line(screen, "lightgray", (x * 40, y * 40),
+                                 (x * 40, y * 40 + 5), 3)
+                pygame.draw.line(screen, "lightgray", (x * 40, y * 40 + 15),
+                                 (x * 40, y * 40 + 25), 3)
+                pygame.draw.line(screen, "lightgray", (x * 40, y * 40 + 35),
+                                 (x * 40, y * 40 + 40), 3)
+              if x < 14 and prev_layer[y][x + 1] != "#":
+                pygame.draw.line(screen, "lightgray", (x * 40 + 40, y * 40),
+                                 (x * 40 + 40, y * 40 + 5), 3)
+                pygame.draw.line(screen, "lightgray",
+                                 (x * 40 + 40, y * 40 + 15),
+                                 (x * 40 + 40, y * 40 + 25), 3)
+                pygame.draw.line(screen, "lightgray",
+                                 (x * 40 + 40, y * 40 + 35),
+                                 (x * 40 + 40, y * 40 + 40), 3)
+              if y > 0 and prev_layer[y - 1][x] != "#":
+                pygame.draw.line(screen, "lightgray", (x * 40, y * 40),
+                                 (x * 40 + 5, y * 40), 3)
+                pygame.draw.line(screen, "lightgray", (x * 40 + 15, y * 40),
+                                 (x * 40 + 25, y * 40), 3)
+                pygame.draw.line(screen, "lightgray", (x * 40 + 35, y * 40),
+                                 (x * 40 + 40, y * 40), 3)
+              if y < 14 and prev_layer[y + 1][x] != "#":
+                pygame.draw.line(screen, "lightgray", (x * 40, y * 40 + 40),
+                                 (x * 40 + 5, y * 40 + 40), 3)
+                pygame.draw.line(screen, "lightgray",
+                                 (x * 40 + 15, y * 40 + 40),
+                                 (x * 40 + 25, y * 40 + 40), 3)
+                pygame.draw.line(screen, "lightgray",
+                                 (x * 40 + 35, y * 40 + 40),
+                                 (x * 40 + 40, y * 40 + 40), 3)
+
+      return screen.copy()
+
+    # draw depth move
+    if animate_layer and key_press_cd > 0:
+      current_layer_map = generate_layer_map(screen.copy())
+      screen.fill("black")
+      animation_frame = 11 - key_press_cd
+      if layer_direction == "lower":
+        prev_layer_map.set_alpha(int(255 - animation_frame * 25.5))
+        place_center(
+            screen,
+            pygame.transform.scale(
+                current_layer_map,
+                (500 + animation_frame * 10, 500 + animation_frame * 10)),
+            (300, 300))
+        place_center(
+            screen,
+            pygame.transform.scale(
+                prev_layer_map,
+                (600 + animation_frame * 10, 600 + animation_frame * 10)),
+            (300, 300))
+      if layer_direction == "upper":
+        current_layer_map.set_alpha(int(animation_frame * 25.5))
+        place_center(
+            screen,
+            pygame.transform.scale(
+                prev_layer_map,
+                (600 - animation_frame * 10, 600 - animation_frame * 10)),
+            (300, 300))
+        place_center(
+            screen,
+            pygame.transform.scale(
+                current_layer_map,
+                (700 - animation_frame * 10, 700 - animation_frame * 10)),
+            (300, 300))
+    else:
+      animate_layer = False
+      prev_layer_map = generate_layer_map(screen)
+
+    # draw ball
+    if animate and key_press_cd > 0:
+      animation_frame = 11 - key_press_cd
+      if direction == "up":
+        pygame.draw.circle(screen, "dodgerblue",
+                           (ball_pos[0] * 40 + 20,
+                            ball_pos[1] * 40 + 60 - animation_frame * 4), 16)
+      if direction == "down":
+        pygame.draw.circle(screen, "dodgerblue",
+                           (ball_pos[0] * 40 + 20,
+                            ball_pos[1] * 40 - 20 + animation_frame * 4), 16)
+      if direction == "right":
+        pygame.draw.circle(screen, "dodgerblue",
+                           (ball_pos[0] * 40 - 20 + animation_frame * 4,
+                            ball_pos[1] * 40 + 20), 16)
+      if direction == "left":
+        pygame.draw.circle(screen, "dodgerblue",
+                           (ball_pos[0] * 40 + 60 - animation_frame * 4,
+                            ball_pos[1] * 40 + 20), 16)
+    else:
+      animate = False
+      pygame.draw.circle(screen, "dodgerblue",
+                         (ball_pos[0] * 40 + 20, ball_pos[1] * 40 + 20), 16)
+
+    # draw hint
     if selected_lvl == 1 and find_ball_pos(lvl) == (ball_pos, ball_depth):
       pygame.draw.rect(screen, "salmon", (170, 280, 260, 40), border_radius=10)
       hint_font = pygame.font.SysFont("Arial", 24)
       hint_text = hint_font.render("Use arrow keys to navigate", True, "white")
       place_center(screen, hint_text, (300, 300))
     if selected_lvl == 2 and find_ball_pos(lvl) == (ball_pos, ball_depth):
-      pygame.draw.rect(screen, "salmon", (100, 240, 400, 120), border_radius=10)
+      pygame.draw.rect(screen,
+                       "salmon", (100, 240, 400, 120),
+                       border_radius=10)
       hint_font = pygame.font.SysFont("Arial", 24)
-      hint_text_1 = hint_font.render("Orange tiles are tunnels between layers", True, "white")
-      hint_text_2 = hint_font.render("Gray boxes show paths of upper layer", True, "white")
-      hint_text_3 = hint_font.render("Use A and Z to go between layers", True, "white")
+      hint_text_1 = hint_font.render("Orange tiles are tunnels between layers",
+                                     True, "white")
+      hint_text_2 = hint_font.render("Gray boxes show paths of upper layer",
+                                     True, "white")
+      hint_text_3 = hint_font.render("Use A and Z to go between layers", True,
+                                     "white")
       place_center(screen, hint_text_1, (300, 260))
       place_center(screen, hint_text_2, (300, 300))
       place_center(screen, hint_text_3, (300, 340))
@@ -199,7 +272,7 @@ while running:
     x, y = ball_pos
 
     # check for win condition
-    if layer[y][x] == "x":
+    if key_press_cd == 0 and layer[y][x] == "x":
       stage = "win"
       continue
 
@@ -208,21 +281,33 @@ while running:
     if key_press_cd == 0:
       if key_pressed[pygame.K_LEFT] and x > 0 and layer[y][x - 1] != "#":
         x -= 1
+        animate = True
+        direction = "left"
         key_press_cd = COOLDOWN
       elif key_pressed[pygame.K_RIGHT] and x < 14 and layer[y][x + 1] != "#":
         x += 1
+        animate = True
+        direction = "right"
         key_press_cd = COOLDOWN
       elif key_pressed[pygame.K_UP] and y > 0 and layer[y - 1][x] != "#":
         y -= 1
+        animate = True
+        direction = "up"
         key_press_cd = COOLDOWN
       elif key_pressed[pygame.K_DOWN] and y < 14 and layer[y + 1][x] != "#":
         y += 1
+        animate = True
+        direction = "down"
         key_press_cd = COOLDOWN
       elif key_pressed[pygame.K_a] and can_go_next_layer(x, y):
         ball_depth += 1
+        animate_layer = True
+        layer_direction = "lower"
         key_press_cd = COOLDOWN
       elif key_pressed[pygame.K_z] and can_go_prev_layer(x, y):
         ball_depth -= 1
+        animate_layer = True
+        layer_direction = "upper"
         key_press_cd = COOLDOWN
       elif key_pressed[pygame.K_ESCAPE]:
         stage = "pause"
@@ -236,7 +321,7 @@ while running:
   if stage == "pause":
     # dialog
     pygame.draw.rect(screen, "white", (200, 200, 200, 200), border_radius=10)
-    pygame.draw.rect(screen, "darkgray", (200, 200, 200, 200), 3)
+    pygame.draw.rect(screen, "gray", (200, 200, 200, 200), 3)
 
     # pause text
     font = pygame.font.SysFont("Arial", 36)
@@ -273,29 +358,37 @@ while running:
   if stage == "win":
     # dialog
     pygame.draw.rect(screen, "white", (200, 200, 200, 200), border_radius=10)
-    pygame.draw.rect(screen, "darkgray", (200, 200, 200, 200), 3)
+    pygame.draw.rect(screen, "gray", (200, 200, 200, 200), 3)
 
     # win text
     font = pygame.font.SysFont("Arial", 36)
     win_text = font.render("You win!", True, "darkgreen")
     place_center(screen, win_text, (300, 250))
 
-    if len(lvls) > selected_lvl:
+    has_next_lvl = len(lvls) > selected_lvl
+
+    if has_next_lvl:
       # next level button
-      pygame.draw.rect(screen, "darkgreen", (250, 280, 100, 40), border_radius=10)
+      pygame.draw.rect(screen,
+                       "darkgreen", (250, 280, 100, 40),
+                       border_radius=10)
       font = pygame.font.SysFont("Arial", 20)
       resume_text = font.render("Next level", True, "white")
       place_center(screen, resume_text, (300, 300))
 
       # menu button
-      pygame.draw.rect(screen, "darkgray", (250, 330, 100, 40), border_radius=10)
+      pygame.draw.rect(screen,
+                       "darkgray", (250, 330, 100, 40),
+                       border_radius=10)
       font = pygame.font.SysFont("Arial", 24)
       menu_text = font.render("Menu", True, "white")
       place_center(screen, menu_text, (300, 350))
-    
+
     else:
       # menu button
-      pygame.draw.rect(screen, "darkgreen", (250, 300, 100, 40), border_radius=10)
+      pygame.draw.rect(screen,
+                       "darkgreen", (250, 300, 100, 40),
+                       border_radius=10)
       font = pygame.font.SysFont("Arial", 24)
       menu_text = font.render("Menu", True, "white")
       place_center(screen, menu_text, (300, 320))
@@ -306,7 +399,7 @@ while running:
 
     if mouse_pressed[0] and not mouse_prev_pressed:
       mouse_pos = pygame.mouse.get_pos()
-      if len(lvls) > selected_lvl:
+      if has_next_lvl:
         if 200 <= mouse_pos[0] <= 400 and 280 <= mouse_pos[1] <= 320:
           selected_lvl += 1
           maze_inited = False
@@ -321,6 +414,18 @@ while running:
           continue
 
     mouse_prev_pressed = mouse_pressed[0]
+
+    key_pressed = pygame.key.get_pressed()
+
+    if key_pressed[pygame.K_SPACE]:
+      if has_next_lvl:
+        selected_lvl += 1
+        maze_inited = False
+        stage = "game"
+        continue
+      else:
+        stage = "menu"
+        continue
 
   clock.tick(60)  # limits FPS to 60
 
